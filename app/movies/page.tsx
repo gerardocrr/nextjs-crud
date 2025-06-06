9749b19897771a71a22be9939df373f42e23130b
gerardocrr <gerardo2551.cr@gmail.com>
2025-06-05 20:33:33 -0600

    feat: implement Movies component to fetch and display movie data in a DataTable

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { DataTable } from "@/components/movies-table/data-table";
+import { columns } from "@/components/movies-table/columns";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return <div className="">Movies</div>;
+  const [dataMovies, setDataMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/movies");
+      const data = await res.json();
+      setDataMovies(data);
+    };
+    fetchData();
+  }, []);
+
+  return <DataTable columns={columns} data={dataMovies} />;
 }
