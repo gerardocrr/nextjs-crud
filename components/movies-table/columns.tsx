206fddd4bac187f16f033e25af21356b78dcfd77
gerardocrr <gerardo2551.cr@gmail.com>
2025-06-05 20:24:34 -0600

    feat: create DataTable component with sorting, filtering, and pagination for movie data

---
+++ components/movies-table/columns.tsx
@@ -0,0 +1,94 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ColumnDef } from "@tanstack/react-table";
+import { Movie } from "@/lib/types";
+// import { DialogDelete } from "../DialogDelete";
+
+export const columns: ColumnDef<Movie>[] = [
+  {
+    accessorKey: "title",
+    header: "Title",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("title")}</div>
+    ),
+  },
+  {
+    accessorKey: "year",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Year
+          <svg
+            className="w-5 h-5 text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m8 15 4 4 4-4m0-6-4-4-4 4"
+            />
+          </svg>
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("year")}</div>,
+  },
+  {
+    accessorKey: "director",
+    header: "Director",
+    cell: ({ row }) => <div>{row.getValue("director")}</div>,
+  },
+  {
+    accessorKey: "poster",
+    header: () => <div className="text-center">Poster</div>,
+    cell: ({ row }) => (
+      <div className="flex justify-center">
+        <img src={row.getValue("poster")} alt="movie image" width={"10%"} />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "id",
+    header: () => <div className="text-right">Actions</div>,
+    enableHiding: false,
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-end gap-2">
+          <Link href={`/update-movie/${row.getValue("id")}`}>
+            <Button variant="outline" size="icon">
+              <svg
+                className="w-5 h-5 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
+                />
+              </svg>
+            </Button>
+          </Link>
+
+          {/* <DialogDelete table={"movies"} id={row.getValue("id")} /> */}
+        </div>
+      );
+    },
+  },
+];
